1e7b0b0cb0c6a84feb03263ff66c3e926ea28506
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2011-08-08 16:08:55 -0500

    fix anchors for FAQ

--- themes/ibp_theme/js/ibp_faq.js
+++ themes/ibp_theme/js/ibp_faq.js
@@ -9,19 +9,35 @@ Drupal.behaviors.ibp_faq = function(context) {
 	$('<div class="faq-category">').append(headers).appendTo('.faq');
 	var pages = $('<div class="faq-category-pages">').appendTo('.faq');
 	
-	var handler = function() {
+	var handler = function(e) {
 		$('.faq-qa-header.selected').removeClass('selected');
 		$(this).addClass('selected');
 		var index = $(this).data('faq-category-page');
-		$('.faq-category-page').hide().eq(index).show();
+		var page = $('.faq-category-page').hide().eq(index);
+		var hash = page[0].id;
+		window.location.hash = hash;
+		page.show();
 	}
 	
 	for (var i = 0; i < qas_top.length; i++) {
-		$('<div class="faq-category-page">').append(qas_top[i]).append(qas[i]).appendTo(pages).hide();
+		var id = qas[i].id;
+		qas[i].id = "";
+		$('<div class="faq-category-page">').attr("id",id).append(qas_top[i]).append(qas[i]).appendTo(pages).hide();
 		$(headers[i]).data('faq-category-page', i).bind('click', handler);
 	}
 	
 	$('.faq-answer').append('<a class="top-link" href="#">Back to top</a>');
 	
-	$(headers[0]).trigger('click');
+	var anchor = window.location.hash;
+	if (anchor) {
+		var pages = $('.faq-category-page');
+		for (var i = 0; i < pages.length; i++) {
+			if (anchor == '#'+pages[i].id) {
+				setTimeout(function() {$(headers[i]).trigger('click')},250);
+				return;
+			}
+		}
+	} else {
+		setTimeout(function() {$(headers[0]).trigger('click')},250);
+	}
 };
