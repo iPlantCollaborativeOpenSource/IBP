b3f68e51729b2fe08f438e96552330ea1e4fbd46
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2011-10-12 18:02:42 -0500

    sidebar menu theme/action

--- themes/ibp_theme/js/ibp-menu-blocks.js
+++ themes/ibp_theme/js/ibp-menu-blocks.js
@@ -3,14 +3,21 @@ Drupal.behaviors.ibp_menu_blocks = function(context) {
 		var menu = $(this);
 		menu.find('li:not(.active-trail).expanded').removeClass('expanded');
 		menu.find('li:not(.active-trail) ul').hide();
-		menu.find('a').bind('click', function() {
+		$('a', menu).each(function(i,o) {
+			var a = $(o);
+			var exp = $('<a class="menu-expander">').attr('href', a.attr('href'));
+			a.append(exp);
+		});
+		menu.find('a.menu-expander').bind('click', function(e) {
+			e.stopPropagation();
 			var $this = $(this);
-			var li = $this.parent();
-			if (li.find('ul').length > 0) {
+			var li = $this.parent().parent(),
+					ul = li.find('ul:first');
+			if (ul.length > 0) {
 				if (li.hasClass('expanded')) {
-					li.find('ul').slideUp(function() {li.removeClass('expanded');});
+					ul.slideUp(function() {li.removeClass('expanded');});
 				} else {
-					li.find('ul').slideDown();
+					ul.slideDown();
 					li.addClass('expanded')
 				}
 				return false;
