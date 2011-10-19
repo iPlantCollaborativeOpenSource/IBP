a39bd695910d95af352115537093b31f3c34ec41
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2011-10-19 16:53:42 -0500

    search box

--- themes/ibp_theme/js/messages.js
+++ themes/ibp_theme/js/messages.js
@@ -3,5 +3,19 @@
 		$('.messages.warning, .messages.status').attr('title', 'Click to dismiss').click(function() {
 			$(this).animate({'opacity':'hide','height':'hide'}, function() {$(this).remove()});
 		});
+		
+		$('#edit-search-theme-form-1').bind('focus', function() {
+			var $this = $(this),
+					val = $this.val();
+			if (val == 'Search IBP') {
+				$this.val('').removeClass('default');
+			}
+		}).bind('blur', function() {
+			var $this = $(this),
+					val = $this.val();
+			if (val == '') {
+				$this.val('Search IBP').addClass('default');
+			}
+		}).trigger('blur');
 	});
 })(jQuery);
