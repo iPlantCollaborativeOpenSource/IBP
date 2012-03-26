ec9653311170524a4ef03d80292165299cefcce3
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2012-03-26 11:38:16 -0500

    fixed js

--- modules/fileversion/expand_collapse.js
+++ modules/fileversion/expand_collapse.js
@@ -1,3 +1,3 @@
-Drupal.behaviors.example_fileversion = function(context) {
-	$('a.toggle-version-history', context).bind('click', function() { $(this).next('.example-version-history').slideToggle(); return false;});
+Drupal.behaviors.fileversion_fileversionList = function(context) {
+	$('a.toggle-version-history', context).bind('click', function() { $(this).next('.fileversion-version-history').slideToggle(); return false;});
 };
